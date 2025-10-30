8fe76d4c7e3c5b63c126b0c385154453fb145f54
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2025-10-30 18:08:38 -0400

    Added Outline

--- Game/Interactive Objects/pickupable_weapon.gd
+++ Game/Interactive Objects/pickupable_weapon.gd
@@ -2,11 +2,14 @@ extends RigidBody2D
 class_name PickupableWeapon
 
 @export var id: StringName = &'None'
+@export var outlined_node: Node2D
 
 var highlighted: bool = false
 
-func _physics_process(delta: float) -> void:
-	modulate.v = 1.5 if highlighted else 0.4
+func _physics_process(_delta: float) -> void:
+	if outlined_node != null:
+		assert(outlined_node.material is ShaderMaterial, "Outlined Node for a Pickupable (" + id + ") must have an \"outline\" shader!")
+		outlined_node.material.set_shader_parameter("active", highlighted)
 
 func _ready() -> void:
 	Registry.register_pickupable(self)
