cc0ca7e18b2419edb6ba63f6617b079cfd162a62
Boxypluto <122708916+Boxypluto@users.noreply.github.com>
2025-11-06 11:16:51 -0500

    Hit Effect Added

--- Game/Enemy/hit_flasher.gd
+++ Game/Enemy/hit_flasher.gd
@@ -2,13 +2,24 @@ extends Timer
 class_name HitFlasher
 
 @export var to_flash: CanvasItem
+var sprite: Sprite2D
+
+const HIT_EFFECT = preload("uid://crhrgpg48gjpt")
 
 func _ready() -> void:
 	one_shot = true
 	wait_time = 0.1
-	timeout.connect(func(): to_flash.material.set_shader_parameter("active", false))
+	timeout.connect(func():
+		to_flash.material.set_shader_parameter("active", false)
+		sprite.visible = false
+		)
+	sprite = Sprite2D.new()
+	sprite.texture = HIT_EFFECT
+	get_parent().add_child.call_deferred(sprite)
+	sprite.visible = false
 
 func do_flash():
 	stop()
+	sprite.visible = true
 	to_flash.material.set_shader_parameter("active", true)
 	start()
